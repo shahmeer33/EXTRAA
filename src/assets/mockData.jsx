4e2b891e8577ba475fbd1c9ef3b8fe7f0100e282
shahmeer33 <shahmeer.humayun2003@gmail.com>
2025-07-28 04:37:20 -0700

    Initial commit

--- src/assets/mockData.jsx
+++ src/assets/mockData.jsx
@@ -1,15 +1,12 @@
-import Image1 from "./Images/bag.jpg"
-import Image2 from "./Images/sandle.jpg"
-import Image3 from "./Images/watch.jpg"
-import Image4 from "./Images/cap.jpg"
-import Image5 from "./Images/boot1.jpg"
-import Image6 from "./Images/dove.jpg"
-import Image7 from "./Images/headphone.jpg"
-import Image8 from "./Images/robot.jpg"
-import Image9 from "./Images/shirt.jpg"
-
-
-
+// import Image1 from "./Images/bag.jpg"
+// import Image2 from "./Images/sandle.jpg"
+// import Image3 from "./Images/watch.jpg"
+// import Image4 from "./Images/cap.jpg"
+// import Image5 from "./Images/boot1.jpg"
+// import Image6 from "./Images/dove.jpg"
+// import Image7 from "./Images/headphone.jpg"
+// import Image8 from "./Images/robot.jpg"
+// import Image9 from "./Images/shirt.jpg"
 
 export const Categories=[
 "Electroncis",
